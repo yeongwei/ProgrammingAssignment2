0029b508abdb56b97f02424ba03d740b8e3e8dd3
yeongwei <ywlai@origineit.com>
2015-05-20 14:35:31 +0800

    Coursera | Data Science | R Programming | Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,93 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a Matrix object with enriched setters and getters
+##
+## Args:
+##  x: Input of matrix class
+##
+## functions:
+##  getMatrix: Returns the matrix within the object
+##  setMatric: Sets a valid matrix for the object else defaults to empty matrix
+##  getInverse: Returns the inverse matrix
+##  setInverse: Sets the inverse matrix; If return is TRUE then return the value
+##
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { 
+  # Privates
+  isValidInput <- function(input) {
+    if (is.null(input)) {
+      print("Error! Please supply a matrix argument.")
+      x <<- matrix()
+      return(FALSE)
+    }
+    
+    if (class(input) != "matrix") {
+      print("Error! Please supply a matrix argument.")
+      x <<- matrix()
+      return(FALSE)
+    }
+    
+    return(TRUE)
+  }
+  
+  # Constructor
+  isValidInput(x)
+  inverse <- NULL
+  
+  # Publics
+  getMatrix <- function() {
+    x
+  }
+  
+  setMatrix <- function(m = matrix()) {
+    if (isValidInput(m)) {
+      x <<- m
+    } else {
+      x <<- matrix()
+    }
+    inverse <<- NULL
+  }
+  
+  getInverse <- function() {
+    inverse
+  }
+  
+  setInverse <- function(i, return = FALSE) {
+    ret <- NULL
+    if (isValidInput(i)) {
+      inverse <<- i
+      ret <- inverse
+    } else {
+      ret <- NA
+    }
+    
+    if (return) {
+      return(ret)
+    }
+  }
+  
+  # Accessors
+  accessors <- list(getMatrix = getMatrix, setMatrix = setMatrix,
+       getInverse = getInverse, setInverse = setInverse)
+  return(accessors)
 }
 
 
-## Write a short comment describing this function
+## Compute the inverse matrix based on the given cachable matrix object
+## If inverse matrix is null then compute
+##
+## Args:
+##  x: Cachable matrix object to perform inverse matrix computation
+##
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if (is.null(x)) {
+    print("Error! Please supply a Cachable matrix.")
+    return(NULL)  
+  }
+  
+  inverse <- x$getInverse()
+  if (is.null(inverse)) {
+    inverse<-x$setInverse(solve(x$getMatrix()), TRUE)
+  }  
+  
+  return(inverse)
 }
